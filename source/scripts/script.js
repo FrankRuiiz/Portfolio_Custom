14b66379789d22e7ffa9aff236093b17f21a2fd6
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-05-30 00:10:41 -0700

    finished basic toggle functionality

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,2 +1,8 @@
 
-console.log('Hello Dude');
+
+$('.toggle-nav').click(function(e) {
+    e.preventDefault();
+    console.log(this);
+    $(this).toggleClass('active');
+    $('.menu ul').toggleClass('active');
+});
