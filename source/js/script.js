71c5e333218e349db900e90141977a4e4abad3a8
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-06-05 20:29:03 -0700

    testing header and nav responsiveness

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,24 @@
 
-
+/** Navigation **/
 $('.toggle-nav').click(function(e) {
     e.preventDefault();
     console.log(this);
     $(this).toggleClass('active');
     $('.menu ul').toggleClass('active');
 });
+
+
+
+/** Header **/
+
+$(function() {
+    var $liElements = $('#intro ul li');
+    var lastElem = null;
+    $liElements.
+    each(function() {
+      if (lastElem && lastElem.offset().top != $(this).offset().top) {
+          $(lastElem).addClass('no-bullet');
+      }
+      lastElem = $(this);
+    }).last().addClass('no-bullet');
+})();
