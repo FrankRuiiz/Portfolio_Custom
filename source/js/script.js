1e01f9ca8dccd92f79df96dd7c7f347e32abc1ad
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-06-26 23:07:28 -0700

    working on fixing the nave bug

--- source/js/script.js
+++ source/js/script.js
@@ -1,24 +1,31 @@
+!(function ($) {
+    /** Navigation **/
+    $('.toggle-nav').click(function(e) {
+        e.preventDefault();
+        console.log(this);
+        // $(this).toggleClass('active');
+        $('.menu ul').slideToggle('active');
+    });
 
-/** Navigation **/
-$('.toggle-nav').click(function(e) {
-    e.preventDefault();
-    console.log(this);
-    $(this).toggleClass('active');
-    $('.menu ul').slideToggle('active');
-});
+    $(window).on('resize', function() {
+       if($(this).width() >= 768) {
+           $('.menu ul').removeClass('active');
+       }
+    });
 
 
 
-/** Header **/
+    /** Header **/
 
-$(function() {
-    var $liElements = $('.intro ul li');
-    var lastElem = null;
-    $liElements.
-    each(function() {
-      if (lastElem && lastElem.offset().top != $(this).offset().top) {
-          $(lastElem).addClass('no-bullet');
-      }
-      lastElem = $(this);
-    }).last().addClass('no-bullet');
-});
+    $(function() {
+        var $liElements = $('.intro ul li');
+        var lastElem = null;
+        $liElements.
+        each(function() {
+          if (lastElem && lastElem.offset().top != $(this).offset().top) {
+              $(lastElem).addClass('no-bullet');
+          }
+          lastElem = $(this);
+        }).last().addClass('no-bullet');
+    });
+})(jQuery);
--- source/js/smooth-scroll.js
+++ source/js/smooth-scroll.js
@@ -1,22 +1,20 @@
-
-(function() {
-    $(function() {
-        // get all a elements that contains # in href, not including the ones that are set equal to #
-        // sets a click handler to these a elements
-        $('a[href*="#"]:not([href="#"])').click(function() {
-            // check if the location and hostname are equal to those in the element clicked upon
-            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-                // target is the elememnt on the page with the id corresponding to the href of the a element
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-                console.log(target.length);
-                if (target.length) {
-                    $('html, body').animate({
-                        scrollTop: target.offset().top
-                    }, 800);
-                    return false;
-                }
+/** Smooth Scrolling **/
+!(function ($) {
+    // get all a elements that contains # in href, not including the ones that are set equal to #
+    // sets a click handler to these a elements
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        // check if the location and hostname are equal to those in the element clicked upon
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            // target is the elememnt on the page with the id corresponding to the href of the a element
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            console.log(target.length);
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 800);
+                return false;
             }
-        });
+        }
     });
-})();
+})(jQuery);
